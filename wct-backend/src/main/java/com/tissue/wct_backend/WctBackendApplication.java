d40f5a3e3ab04dbc8f52a547a40f18b73bdd6443
Emam Hasan <59755463+EmamHasan@users.noreply.github.com>
2025-02-04 00:05:39 +0600

    Backend Initialized

---
+++ wct-backend/src/main/java/com/tissue/wct_backend/WctBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tissue.wct_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WctBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WctBackendApplication.class, args);
+	}
+
+}
